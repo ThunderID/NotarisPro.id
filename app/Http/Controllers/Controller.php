1d8394a9867646a0f2617707b8465e7cc56473ca
budi7 <budi-purnomo@outlook.com>
2017-04-11 18:34:39 +0700

    home controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,81 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use Request, Redirect, App;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	//public page data
+	protected $page_attributes;
+	protected $page_datas;
+
+	function __construct() 
+	{
+		// sets params
+		$this->page_attributes 			= new \Stdclass;
+		$this->page_datas 				= new \Stdclass;
+
+		// auto getter helper
+		$this->getSearch();
+	}   
+
+	public function generateView(){
+		return $this->view
+            ->with('page_attributes', $this->page_attributes)
+			->with('page_datas', $this->page_datas)
+			;
+	} 
+
+	public function generateRedirect($route_to){
+
+		if(isset($this->page_attributes->msg['error'])){
+			if(count($this->page_attributes->msg['error']) > 0){
+				// return Redirect::back()
+				return Redirect::back()
+						->withInput(Request::all())
+						->withErrors($this->page_attributes->msg['error'])
+						;
+			}
+		}
+
+		if(isset($this->page_attributes->msg['warning'])){
+			return Redirect::to($route_to)
+					->with('msg', $this->page_attributes->msg)
+					;
+		}
+
+		if(isset($this->page_attributes->msg['info'])){
+			return Redirect::to($route_to)
+					->with('msg', $this->page_attributes->msg)
+					;
+		}
+
+		if(isset($this->page_attributes->msg['success'])){
+			return Redirect::to($route_to)
+					->with('msg', $this->page_attributes->msg)
+					;
+		}
+
+		// no message
+		return Redirect::to($route_to);
+	} 
+
+	//pagination
+	public function paginate($route = null, $count = null, $current = null, $take = 15){
+		//README
+		//$route : route current page. $route = route('admin.product.index')
+		//$count : number of data. $count = count($data)
+		//$current : current page. $current = input::get($page)
+
+		$this->page_attributes->paging = new LengthAwarePaginator($count, $count, $take, $current);
+	    $this->page_attributes->paging->setPath($route);
+	}
+
+	//search
+	private function getSearch(){
+		// get all input with search input name
+        $this->page_attributes->search    = Request::input('search');
+	}  
 }
---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homeController extends Controller
+{
+    //
+    public function dashboard(){
+		// init
+		$this->page_attributes->title       = 'Data Member';
+
+		//get data from database
+		$this->page_datas->datas            = null;
+
+		//initialize view
+		$this->view                         = view('pages.dashboard.dashboard');
+
+		//function from parent to generate view
+		return $this->generateView();  
+    }
+}
