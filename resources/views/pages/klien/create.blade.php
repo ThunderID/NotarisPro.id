b97b615c83728c230b767a7aa3ae1587c81fd81a
budi7 <budi-purnomo@outlook.com>
2017-04-18 10:07:00 +0700

    fixed create klien styling

--- resources/views/pages/klien/create.blade.php
+++ resources/views/pages/klien/create.blade.php
@@ -13,7 +13,7 @@
 
 		<div class="container">
 
-			<div class="col-6 offset-3">
+			<div class="col-12 col-sm-10 col-md-8 col-xl-6 offset-sm-1 offset-md-2 offset-xl-3 input_panel">
 
 				@component('components.form', [ 
 					'data_id' 		=> $page_datas->id,
@@ -22,98 +22,108 @@
 				])
 
 					<div class="row">
-						<h4 class="title" style="margin-left: 0px;">{{$page_attributes->title}}</h4>
+						<div class="col-12">
+							<h4 class="title" style="margin-left: 0px;">{{$page_attributes->title}}</h4>
+						</div>
 					</div>
 
 					<fieldset class="form-group">
-						<label class=" control-label" for="textinput">Nama</label>  
 						<div class="row">
-							<div class="col-md-5">
+							<div class="col-12">
+								<label class="control-label" for="nama">Nama</label>  
 								<input id="textinput" name="nama" class="form-control" type="text">
+							</div>
+						</div>
+					</fieldset>
+
+					<fieldset class="form-group">
+						<div class="row">
+							<div class="col-7">
+								<label class="control-label" for="tempat_lahir">Tempat Lahir</label>  
+								<input id="textinput" name="tempat_lahir" class="form-control" type="text">
+							</div>
+							<div class="col-5">
+								<label class="control-label" for="tanggal_lahir">Tanggal Lahir</label>  
+								<input id="textinput" name="tanggal_lahir" class="form-control" type="text">
+							</div>
+						</div>
+					</fieldset>	
+
+					<fieldset class="form-group">
+						<div class="row">
+							<div class="col-8">
+								<label class=" control-label" for="pekerjaan">Pekerjaan</label>  
+								<input id="textinput" name="pekerjaan" class="form-control" type="text">
+							</div>
+						</div>
+					</fieldset>
+
+					<fieldset class="form-group">
+						<div class="row">
+							<div class="col-6">
+								<label class=" control-label" for="nomor_ktp">Nomor KTP</label>  
+								<input id="textinput" name="nomor_ktp" class="form-control" type="text">
 							</div>
 						</div>
 					</fieldset>					
 
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">Tempat Lahir</label>  
-							<input id="textinput" name="tempat_lahir" class="form-control" type="text">
+					<fieldset class="form-group">
+						<div class="row">
+							<div class="col-12">
+								<label class=" control-label" for="alamat[alamat]">Alamat</label>  
+								<input id="textinput" name="alamat[alamat]" class="form-control" type="text">
+							</div>
 						</div>
-					</div>		
+					</fieldset>		
 
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">Tanggal Lahir</label>  
-							<input id="textinput" name="tanggal_lahir" class="form-control" type="text">
+					<fieldset class="form-group">
+						<div class="row">
+							<div class="col-3">
+								<label class=" control-label" for="alamat[rt]">RT</label>  
+								<input id="textinput" name="alamat[rt]" class="form-control" type="text">
+							</div>
+							<div class="col-3">
+								<label class=" control-label" for="alamat[rw]">RW</label>  
+								<input id="textinput" name="alamat[rw]" class="form-control" type="text">
+							</div>	
+							<div class="col-6">
+								<label class=" control-label" for="alamat[regensi]">Regensi</label>  
+								<input id="textinput" name="alamat[regensi]" class="form-control" type="text">
+							</div>
 						</div>
-					</div>	
+					</fieldset>		
 
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">Pekerjaan</label>  
-							<input id="textinput" name="pekerjaan" class="form-control" type="text">
+					<fieldset class="form-group">
+						<div class="row">
+							<div class="col-8">
+								<label class=" control-label" for="alamat[provinsi]">Provinsi</label>  
+								<input id="textinput" name="alamat[provinsi]" class="form-control" type="text">
+							</div>
 						</div>
-					</div>	
+					</fieldset>		
 
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">Nomor KTP</label>  
-							<input id="textinput" name="nomor_ktp" class="form-control" type="text">
+					<fieldset class="form-group">
+						<div class="row">
+							<div class="col-6">
+								<label class=" control-label" for="alamat[negara]">Negara</label>  
+								<input id="textinput" name="alamat[negara]" class="form-control" type="text">
+							</div>
 						</div>
-					</div>		
+					</fieldset>		
 
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">Alamat</label>  
-							<input id="textinput" name="alamat[alamat]" class="form-control" type="text">
-						</div>
-					</div>	
-
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">RT</label>  
-							<input id="textinput" name="alamat[rt]" class="form-control" type="text">
-						</div>
-					</div>	
-
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">RW</label>  
-							<input id="textinput" name="alamat[rw]" class="form-control" type="text">
-						</div>
-					</div>		
-
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">Regensi</label>  
-							<input id="textinput" name="alamat[regensi]" class="form-control" type="text">
-						</div>
-					</div>		
-
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">Provinsi</label>  
-							<input id="textinput" name="alamat[provinsi]" class="form-control" type="text">
-						</div>
-					</div>										
-
-					<div class="row">
-						<div class="form-group">
-							<label class=" control-label" for="textinput">Negara</label>  
-							<input id="textinput" name="alamat[negara]" class="form-control" type="text">
-						</div>
-					</div>
-
-					<div class="row">
+					<fieldset class="form-group">
 						</br>
-					</div>
+					</fieldset>
 
-					<div class="row">
-						<div class="form-group">
-								<a href="{{ isset($page_datas->id) ? route('klien.show', ['id' => $page_datas->id]) : route('klien.index') }}" type="button" class="btn btn-default">Cancel</a>
+
+					<fieldset class="form-group">
+						<div class="row">
+							<div class="col-6">
+								<a href="{{ isset($page_datas->id) ? route('klien.show', ['id' => $page_datas->id]) : route('klien.index') }}" type="button" class="btn btn-secondary">Cancel</a>
 								<button type="submit" class="btn btn-primary">Save</button>
+							</div>
 						</div>
-					</div>
+					</fieldset>		
 
 				@endcomponent	
 
