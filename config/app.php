b07f69e05e72719f728226557166d2b3abb22ba3
budi7 <budi-purnomo@outlook.com>
2017-04-18 08:51:12 +0700

    fixed app.config

--- config/app.php
+++ config/app.php
@@ -180,7 +180,7 @@ return [
 
         TWeb\WebServiceProvider::class,
         Jenssegers\Mongodb\MongodbServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
+        // Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -230,8 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
+        // 'Form' => Collective\Html\FormFacade::class,
+        // 'Html' => Collective\Html\HtmlFacade::class,
 
         'TAuth' => TFacades\TAuth::class,
     ],
