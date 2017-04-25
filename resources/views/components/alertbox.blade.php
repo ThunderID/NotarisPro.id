637e52109a4fc999cfe26fd744fae34256551c1e
budi7 <budi-purnomo@outlook.com>
2017-04-25 14:23:49 +0700

    fixed styling alertbox

--- resources/views/components/alertbox.blade.php
+++ resources/views/components/alertbox.blade.php
@@ -1,17 +1,15 @@
 @if(Session::has('msg'))
-	<div class="card-block pt-0 pb-0">
-		<div class="row p-b-md">
-		    <div class="col-lg-12">
-		        <div class="alert alert-{{ key(Session::get('msg')) }} alert-dismissable mb-0 mt-0">
-		            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    	            @foreach (Session::get('msg')[key(Session::get('msg'))] as $msg)
-		                <p class="mb-1">{!! $msg !!}</p>
-		            @endforeach
-    	            @foreach ($errors->all('<p>:message</p>') as $error)
-		                {!! $error !!}
-		            @endforeach
-		        </div>
-		    </div>
-		</div>
+	<div class="card-block" style="margin-bottom: 15px;">
+	    <div class="col-lg-12">
+	        <div class="alert alert-{{ key(Session::get('msg')) }} alert-dismissable mb-0 mt-0">
+	            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	            @foreach (Session::get('msg')[key(Session::get('msg'))] as $msg)
+	                <p class="mb-1">{!! $msg !!}</p>
+	            @endforeach
+	            @foreach ($errors->all('<p>:message</p>') as $error)
+	                {!! $error !!}
+	            @endforeach
+	        </div>
+	    </div>
 	</div>
 @endif
