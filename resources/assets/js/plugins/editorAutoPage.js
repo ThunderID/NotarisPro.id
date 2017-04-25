cd62e5a01b2df7ba23f03cf2d95ebe0b392d5dfd
budi7 <budi-purnomo@outlook.com>
2017-04-25 15:09:58 +0700

    fixed aut page break on template show

--- resources/assets/js/plugins/editorAutoPage.js
+++ resources/assets/js/plugins/editorAutoPage.js
@@ -37,26 +37,43 @@ window.editorPaging = new function() {
 
     this.autoAdjustHeight = function (target, target_padding, editor, editor_padding) {
 
-    	/* init */
+
+    	/* init 
     	var inner_h = editor.height() + editor_padding  + target_padding;
     	var outer_h = this.pageHeight;
     	var gap = outer_h - inner_h;
 
-    	/* measure ideal height */
+    	/* measure ideal height 
 		do {
 			if(gap < 0){
 				this.autoPageBreak(outer_h);
 
-			    /* add new page */
+			    /* add new page 
 			    outer_h = outer_h + this.pageHeight;
 			    gap = outer_h - inner_h;
 			}
 		}
 		while (gap < 0);
 
-		/* apply height */
+		/* apply height 
     	target.css('min-height', outer_h);
+    	*/
 
+    	var inner_h = editor.height() + editor_padding  + target_padding;
+    	var outer_h = this.pageHeight;
+
+		do {
+			if(outer_h < inner_h){
+				this.autoPageBreak(outer_h);
+
+			    /* add new page */
+			    outer_h = outer_h + this.pageHeight;
+			}
+		}
+		while (outer_h < inner_h);    	
+
+		/* apply height */ 
+    	target.css('min-height', outer_h);
     };
 
 
