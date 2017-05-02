b6a6d742c1189c8604c1d5c1bdd9e34667f1a948
chelsymooy <chelsymooy1108@gmail.com>
2017-05-02 17:17:45 +0700

    fixing stripes

--- app/Http/Controllers/temporaryParagraphController.php
+++ app/Http/Controllers/temporaryParagraphController.php
@@ -13,6 +13,8 @@ class temporaryParagraphController extends Controller
 		parent::__construct();
 		
 		$this->query            = $query;
+		$this->ol     	       	= 0;
+		$this->closeol			= 0;
 	}    
 
 	public function form()
@@ -46,68 +48,115 @@ class temporaryParagraphController extends Controller
 
 	public function get()
 	{
-		if(Session::has('texteddd'))
+		// login info
+		$credentials 	= ['email' => 'admin@notaris.id', 'password' => 'admin'];
+
+		$login 			= \TAuth::login($credentials);
+
+
+		$klien 			= \TKlien\Klien\Models\Klien::first()->toArray();
+
+		$akta 			= new \TQueries\Akta\DaftarAkta;
+		$akta 			= $akta->detailed('AFEE0840-9371-4707-A48B-BD753E545A56');
+
+
+		// $new_text		= $this->count_stripes($akta);
+
+		foreach ($akta['paragraf'] as $key => $value) 
 		{
-			$text 		= Session::get('texteddd');
+			// $value['konten']					= str_replace('&nbsp;', ' ', $value['konten']);
+			$akta['paragraf'][$key]['konten']	= $this->htmlwrap($value['konten'],72);
 
-			$new_text 	= [];
-
-			foreach ($text as $key => $value) 
+			if($key==12)
 			{
-				$text_length 	= strlen($value);
-				$start_point 	= 0;
-				$end_point 		= min(72, strlen($value));
-
-				if($end_point < 72)
-				{
-					$dots 				= 72 - $end_point;
-					$chunked 			= $value;
-
-					foreach (range(0, $dots-1) as $key2) 
-					{
-						$chunked 		= $chunked.'-';
-					}
-
-					$new_text[$key][]	= $chunked;
-				}
-				else
-				{
-					//setiap karakter, 
-					do
-					{
-						//jika spasi ketemu, tandai titik akhir
-						if(isset($value[$end_point]) && str_is($value[$end_point], ' '))
-						{
-
-							$chunked			=  substr($value, $start_point, $end_point - $start_point);
-							
-							$dots 				= 72 - ($end_point - $start_point);
-
-							if($dots > 0)
-							{
-								foreach (range(0, $dots-1) as $key2) 
-								{
-									$chunked 		= $chunked.'-';
-								}
-							}
-
-							$new_text[$key][]	= $chunked;
-							
-							$start_point 		= $end_point+1;
-							$end_point 			= $start_point + 72;
-						}
-						//jika titik akhir bukan spasi, mundur titik akhir sampai ketemu spasi
-						else
-						{
-							$end_point  = $end_point - 1;
-						}
-					}
-					while ($end_point < $text_length && $end_point >0); 
-				}
+				// dd($akta['paragraf'][$key]['konten']);
 			}
 		}
 
-		return view('temporary.show')->with('new_text', $new_text);
+		return view('temporary.show')->with('new_text', $akta);
 	}
 
+	function htmlwrap(&$str, $maxLength, $char='<br/>')
+	{
+		//count indent
+		$ol 		= substr_count($str, '<ol>');
+		$closeol  	= substr_count($str, '</ol>');
+
+		if($ol > 0 && $this->ol > 0)
+		{
+			$this->ol 	= $this->ol + 1;
+			$maxLength 	= $maxLength - ($this->ol* 5);
+		}
+		
+		if($closeol > 0 && $this->ol > 0)
+		{
+			$maxLength = $maxLength - ($this->ol* 5);
+			$this->ol 	= 0;
+		}
+
+		if($ol > 0 && $this->ol == 0)
+		{
+			$maxLength = $maxLength - (5);
+			$this->ol 	= $this->ol + 1;
+		}
+
+		$str 	= strip_tags($str,"<p><h1><h2><h3><ul><ol><li><b><i>"); 
+		// $str 	= str_replace('</span>', ' ', $str);
+		$str 	= str_replace('&nbsp;', ' ', $str);
+	    $count 	= 0;
+	    $newStr = '';
+	    $openTag 	= false;
+	    $lenstr 	= strlen($str);
+	    for($i=0; $i<$lenstr; $i++){
+	        $newStr .= $str{$i};
+	        if($str{$i} == '<'){
+	            $openTag = true;
+	            continue;
+	        }
+	        if(($openTag) && ($str{$i} == '>')){
+	            $openTag = false;
+	            continue;
+	        }
+	        if(!$openTag){
+	            if($str{$i} == ' '){
+	                if ($count == 0) {
+	                    $newStr = substr($newStr,0, -1);
+	                    continue;
+	                } else {
+	                    $lastspace = $count + 1;
+	                }
+	            }
+	            $count++;
+	            if($count==$maxLength){
+	                if ($str{$i+1} != ' ' && $lastspace && ($lastspace < $count)) {
+	                    $tmp = ($count - $lastspace)* -1;
+	                	$stripes = '';
+	                    foreach (range(0, $tmp * 1) as $key) 
+	                    {
+	                    	$stripes = $stripes.'-';
+	                    }
+	                    
+	                    $newStr = substr($newStr,0, $tmp).$stripes . $char . substr($newStr,$tmp);
+
+	                    if($tmp==0)
+	                    {
+	                    	dd(substr($newStr,0, $tmp));
+	                    }
+	                    $count = $tmp * -1;
+	                } else {
+	                	foreach (range(0, ($maxLength - $count)) as $key) 
+	                    {
+	                    	$newStr .= '-';
+	                    }
+	                    
+	                    $newStr .= $char;
+	                    $count = 0;
+	                }
+	                $lastspace = 0;
+	            }
+	        }  
+	    }
+
+	    return $newStr;
+	}
 }
