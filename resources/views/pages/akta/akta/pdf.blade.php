641a876054aed2ccd9280284916d5b2df1278627
budi7 <budi-purnomo@outlook.com>
2017-05-15 09:46:08 +0700

    fixed export pdf

--- resources/views/pages/akta/akta/pdf.blade.php
+++ resources/views/pages/akta/akta/pdf.blade.php
@@ -5,9 +5,7 @@
 @endpush
 
 @push('styles')  
-	html{
-		margin:0;
-	}
+	html{ margin: 2cm 1cm 3cm 5cm; }
 
 	p, ul, ol, h5 {
 		margin-top: 0;
@@ -16,11 +14,41 @@
 	body {
 		font-family: 'Inconsolata', monospace !important;
 	}
+
+	@page { margin: 2cm 1cm 3cm 5cm; }
+
+	.header,
+	.footer {
+	    width: 100%;
+	    text-align: left;
+	    position: fixed;
+	    border-top: 1px solid black;
+	    font-size: 0.8rem;
+	}
+	.header {
+	    top: 0px;
+	}
+	.footer {
+	    bottom: 0px;
+	}
+	.pagenum:before {
+	    content: counter(page);
+	}	
 @endpush  
 
 @section('content')
 
-	<div class="form font-editor page-editor" style="width: 21cm; min-height: 29.7cm; background-color: #fff; padding-top: 2cm; padding-bottom: 0cm; padding-left: 5cm; padding-right: 1cm; ">
+	<div class="footer">
+	    <p>
+			{{ $page_datas->notaris['nama'] }}
+	    	<span style="float:right;">Halaman <span class="pagenum"></span>
+	    </p>
+	    <p>
+			Daerah Kerja {{ $page_datas->notaris['daerah_kerja'] }}
+	    </p>
+	</div>
+
+	<div class="form font-editor page-editor" style="width: 21cm; min-height: 29.7cm; background-color: #fff;">
 		<div class="form-group editor">
 			@foreach($page_datas->datas['paragraf'] as $key => $value)
 				{!!$value['konten']!!}
