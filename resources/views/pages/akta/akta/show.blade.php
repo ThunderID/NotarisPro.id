27775c708e58f14659b4b429b6a1b3f522e03cdc
budi7 <budi-purnomo@outlook.com>
2017-05-15 20:10:30 +0700

    fixed versioning style

--- resources/views/pages/akta/akta/show.blade.php
+++ resources/views/pages/akta/akta/show.blade.php
@@ -28,7 +28,7 @@
 							@if(str_is($page_datas->datas['status'], 'pengajuan'))
 								@if($page_datas->datas['total_perubahan'] * 1 == 0)
 									<li class="nav-item">
-										<span class="nav-link">Status : Menunggu Renvoi</span>
+										<!-- <span class="nav-link">Status : Menunggu Renvoi</span> -->
 									</li>
 								@else
 									<li class="nav-item">
