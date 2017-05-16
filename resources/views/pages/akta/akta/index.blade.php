97a024cae69669cbf9674bc4a8035941de09cbe1
chelsy <chelsymooy1108@gmail.com>
2017-05-16 13:53:46 +0700

    fixing search and statuses

--- resources/views/pages/akta/akta/index.blade.php
+++ resources/views/pages/akta/akta/index.blade.php
@@ -37,14 +37,7 @@
 						'title' => 'Filter Akta',
 						'alias' => 'status',
 						'qs'	=> [ 'q','urutkan' ],
-						'lists' => [
-							'semua status' 	=> null,
-							'dalam proses'	=> 'dalam_proses', 
-							'draft' 		=> 'draft',
-							'renvoi' 		=> 'renvoi',
-							'akta' 			=> 'akta',
-							'minuta akta' 	=> 'minuta',
-						]
+						'lists' => \App\Service\Akta\DaftarAkta::statuses()
 					])
 				</div>	
 
