560fd92ea7300b51785e72affa6624dc9e810e7b
chelsymooy <chelsymooy1108@gmail.com>
2017-05-19 08:53:26 +0700

    tag issue

--- app/Service/Akta/DaftarTag.php
+++ app/Service/Akta/DaftarTag.php
@@ -45,7 +45,6 @@ class DaftarTag
 		$dok['notaris']['notaris']	= ['@notaris.nama', '@notaris.daerah_kerja', '@notaris.nomor_sk', '@notaris.tanggal_pengangkatan', '@notaris.alamat', '@notaris.telepon'];
 
 		$dok['objek']	= [];
-
 		//@shgb.nomor
 		foreach ($dokumen_objek as $key => $value) 
 		{
@@ -64,23 +63,22 @@ class DaftarTag
 		{
 			foreach ($value as $key2 => $value2) 
 			{
-				if(isset($doks[$value2]))
+				if(isset($doks[$key2]))
 				{
-					foreach ($doks[$value2] as $key3 => $value3) 
+					foreach ($doks[$key2] as $key3 => $value3) 
 					{
-						$dok["saksi_$key"][$value2][$key3]	= '@saksi.'.$key.'.'.$value2.'.'.$value3;
+						$dok["saksi_$key"][$key2][$key3]	= '@saksi.'.$key.'.'.$key2.'.'.$value3;
 					}
 				}
-				elseif(str_is('ktp_*', $value2))
+				elseif(str_is('ktp_*', $key2))
 				{
 					foreach ($doks['ktp'] as $key3 => $value3) 
 					{
-						$dok["saksi_$key"][$value2][$key3]	= '@saksi.'.$key.'.'.$value2.'.'.$value3;
+						$dok["saksi_$key"][$key2][$key3]	= '@saksi.'.$key.'.'.$key2.'.'.$value3;
 					}
 				}
 			}
 		}
-
 
 		$dok['pihak_1']	= [];
 		//@pihak.1.ktp.nik
@@ -88,18 +86,18 @@ class DaftarTag
 		{
 			foreach ($value as $key2 => $value2) 
 			{
-				if(isset($doks[$value2]))
+				if(isset($doks[$key2]))
 				{
-					foreach ($doks[$value2] as $key3 => $value3) 
+					foreach ($doks[$key2] as $key3 => $value3) 
 					{
-						$dok["pihak_$key"][$value2][$key3]	= '@pihak.'.$key.'.'.$value2.'.'.$value3;
+						$dok["pihak_$key"][$key2][$key3]	= '@pihak.'.$key.'.'.$key2.'.'.$value3;
 					}
 				}
-				elseif(str_is('ktp_*', $value2))
+				elseif(str_is('ktp_*', $key2))
 				{
 					foreach ($doks['ktp'] as $key3 => $value3) 
 					{
-						$dok["pihak_$key"][$value2][$key3]	= '@pihak.'.$key.'.'.$value2.'.'.$value3;
+						$dok["pihak_$key"][$key2][$key3]	= '@pihak.'.$key.'.'.$key2.'.'.$value3;
 					}
 				}
 			}
--- app/Service/Tag/TagService.php
+++ app/Service/Tag/TagService.php
@@ -2,6 +2,8 @@
 
 namespace App\Service\Tag;
 
+use App\Domain\Akta\Models\TipeDokumen;
+use TAuth;
 /**
  * Kelas Navbar
  *
@@ -12,41 +14,22 @@ namespace App\Service\Tag;
 class TagService 
 {
 	/**
-	 * Membuat object asset baru dari data array
+	 * menampilkan data tag untuk kantor tersebut dari model tipe dokumen
 	 *
-	 * @return array $nav
+	 * @return array $isi
 	 */
-	public static function all($number_of_client = 1)
+	public static function all()
 	{
-		//1. dokumen pihak
-		$dok_mention	= [
-			'ktp'				=> ['nik', 'nama', 'tempat_lahir', 'tanggal_lahir', 'jenis_kelamin', 'status_pernikahan', 'pekerjaan', 'kewarganegaraan', 'alamat'],
-			'kk'				=> ['nomor', 'nama_kepala_keluarga', 'alamat', 'dikeluarkan_tanggal'],
-			'paspor'			=> ['nomor', 'stbld', 'nama_semula', 'nama_sekarang', 'tempat_ganti_nama', 'tanggal_ganti_nama'],
-			'akta_lahir'		=> ['nomor', 'stbld', 'nama', 'nama_ayah', 'nama_ibu', 'tempat_lahir', 'tanggal_lahir'],
-			'akta_ganti_nama'	=> ['nomor', 'stbld', 'nama_semula', 'nama_sekarang', 'tempat_ganti_nama', 'tanggal_ganti_nama'],
-			'akta_nikah'		=> ['nomor', 'stbld', 'tanggal_pencatatan', 'tanggal_pernikahan', 'nama_suami', 'nama_istri', 'agama', 'pemuka_agama', 'tempat_upacara_pernikahan', 'nama_notaris', 'nomor_sk_notaris'],
-			'akta_cerai'		=> ['nomor', 'stbld', 'tempat_cerai', 'tanggal_cerai', 'nama_suami', 'nama_istri', 'nomor_akta_perkawinan', 'tanggal_perkawinan'],
-			'akta_kematian'		=> ['nomor', 'stbld', 'tempat_kematian', 'tanggal_kematian', 'nama', 'tempat_lahir', 'tanggal_lahir', 'alamat_terakhir', 'nama_ibu'],
-			'akta_waris'		=> ['nomor', 'stbld', 'nama_ahli_waris', 'tanggal_lahir'],
-			'akta_pendirian'	=> ['nomor', 'stbld', 'nama_pihak'],
-			'pengesahan_depkumham'		=> ['nomor', 'ditetapkan_di', 'ditetapkan_tanggal'],
-			'akta_perubahan_terakhir'	=> ['nomor'],
-			'shgb'				=> ['nomor', 'alamat', 'atas_nama'],
-			'shm'				=> ['nomor', 'alamat', 'atas_nama'],
-			'imb'				=> ['nomor', 'alamat', 'atas_nama'],
-			'pbb'				=> ['nomor', 'alamat', 'nama_wajib_pajak'],
-			'hpl'				=> ['nomor'],
-			'bpkb'				=> ['nomor', 'nomor_polisi', 'merek', 'tipe', 'jenis', 'model', 'tahun_pembuatan', 'tahun_perakitan', 'isi_silinder', 'warna', 'nomor_rangka', 'nomor_mesin', 'jumlah_sumbu', 'jumlah_roda', 'bahan_bakar', 'dikeluarkan_tanggal', 'nama_pemilik', 'alamat_pemilik'],
-			'stnk'				=> ['nomor', 'nomor_polisi', 'merek', 'tipe', 'jenis', 'model', 'tahun_pembuatan', 'tahun_perakitan', 'isi_silinder', 'warna', 'nomor_rangka', 'nomor_mesin', 'jumlah_sumbu', 'jumlah_roda', 'bahan_bakar', 'dikeluarkan_tanggal', 'nama_pemilik', 'alamat_pemilik'],
-		];
+		$tag 	= TipeDokumen::kantor(TAuth::activeOffice()['kantor']['id'])->first();
 
-		return $dok_mention;
+		return $tag['isi'];
 	}
 
 	public static function fill_mention($dokumen_objek, $dokumen_pihak, $dokumen_saksi)
 	{
-		$doks 			= self::all()['isi'];
+		$doks 			= self::all();
+
+		$dokumen_objek 	= ['shgb'];
 
 		//generate docs
 		$dok['akta']['akta']		= ['@akta.nomor', '@akta.tanggal'];
@@ -72,18 +55,18 @@ class TagService
 		{
 			foreach ($value as $key2 => $value2) 
 			{
-				if(isset($doks[$value2]))
+				if(isset($doks[$key2]))
 				{
-					foreach ($doks[$value2] as $key3 => $value3) 
+					foreach ($doks[$key2] as $key3 => $value3) 
 					{
-						$dok["saksi_$key"][$value2][$key3]	= '@saksi.'.$key.'.'.$value2.'.'.$value3;
+						$dok["saksi_$key"][$key2][$key3]	= '@saksi.'.$key.'.'.$key2.'.'.$value3;
 					}
 				}
-				elseif(str_is('ktp_*', $value2))
+				elseif(str_is('ktp_*', $key2))
 				{
 					foreach ($doks['ktp'] as $key3 => $value3) 
 					{
-						$dok["saksi_$key"][$value2][$key3]	= '@saksi.'.$key.'.'.$value2.'.'.$value3;
+						$dok["saksi_$key"][$key2][$key3]	= '@saksi.'.$key.'.'.$key2.'.'.$value3;
 					}
 				}
 			}
@@ -96,18 +79,18 @@ class TagService
 		{
 			foreach ($value as $key2 => $value2) 
 			{
-				if(isset($doks[$value2]))
+				if(isset($doks[$key2]))
 				{
-					foreach ($doks[$value2] as $key3 => $value3) 
+					foreach ($doks[$key2] as $key3 => $value3) 
 					{
-						$dok["pihak_$key"][$value2][$key3]	= '@pihak.'.$key.'.'.$value2.'.'.$value3;
+						$dok["pihak_$key"][$key2][$key3]	= '@pihak.'.$key.'.'.$key2.'.'.$value3;
 					}
 				}
-				elseif(str_is('ktp_*', $value2))
+				elseif(str_is('ktp_*', $key2))
 				{
 					foreach ($doks['ktp'] as $key3 => $value3) 
 					{
-						$dok["pihak_$key"][$value2][$key3]	= '@pihak.'.$key.'.'.$value2.'.'.$value3;
+						$dok["pihak_$key"][$key2][$key3]	= '@pihak.'.$key.'.'.$key2.'.'.$value3;
 					}
 				}
 			}
