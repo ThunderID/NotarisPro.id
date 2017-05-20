bc504e17f8f594fba25f8f90ace3ab07e4241f43
chelsy <chelsymooy1108@gmail.com>
2017-05-20 12:45:09 +0700

    Final aktacontroller

--- app/Http/Controllers/Akta/aktaController.php
+++ app/Http/Controllers/Akta/aktaController.php
@@ -321,7 +321,7 @@ class aktaController extends Controller
 			{
 				foreach ($template['mentionable'] as $key => $value) 
 				{
-					if(!str_is('@notaris.*', $value) && !str_is('@akta.*', $value))
+					if(!str_is('@notaris.*', $value) && !str_is('@akta.nomor', $value))
 					{
 						if(str_is('@objek*', $value))
 						{
@@ -343,13 +343,13 @@ class aktaController extends Controller
 							$prefix 				= explode('.', $prefix);
 							$mentionable[$prefix[0].'_'.$prefix[1]][$prefix[2]][]	= $value;
 						}
-					}
-					else
-					{
-						$prefix 				= str_replace('@', '', $value);
-						$prefix 				= explode('.', $prefix);
-						$mentionable[$prefix[0]][$prefix[0]][]	= $value;
 
+						if(str_is('@akta*', $value))
+						{
+							$prefix 				= str_replace('@', '', $value);
+							$prefix 				= explode('.', $prefix);
+							$mentionable[$prefix[0]][$prefix[0]][]	= $value;
+						}
 					}
 				}
 			}
