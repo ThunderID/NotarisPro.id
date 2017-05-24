3e06976d31bc8140aa65f15bdc546246bded1359
budi7 <budi-purnomo@outlook.com>
2017-05-24 08:45:34 +0700

    fixed update template layout

--- resources/views/templates/basic.blade.php
+++ resources/views/templates/basic.blade.php
@@ -1,14 +1,16 @@
 @extends('layout.base')
 
+<!-- Content -->
+@section('template')
+
 <!-- Header -->
 @include('templates.components.header')
 
-<!-- Content -->
-@section('template')
 <div class="container-fluid" style="padding-top: 54px;">
     @yield('content')           
 </div>
-@stop
 
 <!-- Footer -->
 @include('templates.components.footer')
+
+@stop
